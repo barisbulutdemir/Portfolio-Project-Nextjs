1f028a698af65e0d382bf5b9fd00eedcd14d5622
barisbulutdemir <barisbulutdemir@gmail.com>
2023-06-05 12:56:30 +0200

    2
    22

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,91 +3,47 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./blog.module.css";
 
-const Blog = () => {
+
+
+async function getData(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        next: {revalidate: 10},
+    });
+
+
+    if ( !res.ok) {
+        throw new Error ("Failed to get data");
+    }
+    return res.json();
+}
+
+const Blog = async () => {
+    const data = await getData()
     return (
         <>
             <div className={styles.mainContainer}>
 
-                <Link href="/blog/" className={styles.container} >
 
-                    <div className={styles.imgContainer} >
-                        <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-                               alt=""
-                               width={400}
-                               height={250}
-                               className={styles.image}
-                               />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}> test</h1>
-                        <p className={styles.desc}>desc</p>
+                {data.map(item => (
 
-                    </div>
-                </Link>
-                <Link href="#" className={styles.container} >
+                    <Link key={item.id} href="/blog/testid" className={styles.container} >
 
-                    <div className={styles.imgContainer} >
-                        <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-                               alt=""
-                               width={400}
-                               height={250}
-                               className={styles.image}
-                        />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}> test</h1>
-                        <p className={styles.desc}>desc</p>
+                        <div className={styles.imgContainer} >
+                            <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                                   alt=""
+                                   width={400}
+                                   height={250}
+                                   className={styles.image}
+                            />
+                        </div>
+                        <div className={styles.content}>
+                            <h1 className={styles.title}> {item.title}</h1>
+                            <p className={styles.desc}>{item.title}</p>
 
-                    </div>
-                </Link>
-                <Link href="#" className={styles.container} >
+                        </div>
+                    </Link>
+                ))}
 
-                    <div className={styles.imgContainer} >
-                        <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-                               alt=""
-                               width={400}
-                               height={250}
-                               className={styles.image}
-                        />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}> test</h1>
-                        <p className={styles.desc}>desc</p>
-
-                    </div>
-                </Link>
-                <Link href="#" className={styles.container} >
-
-                    <div className={styles.imgContainer} >
-                        <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-                               alt=""
-                               width={400}
-                               height={250}
-                               className={styles.image}
-                        />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}> test</h1>
-                        <p className={styles.desc}>desc</p>
-
-                    </div>
-                </Link>
-                <Link href="#" className={styles.container} >
-
-                    <div className={styles.imgContainer} >
-                        <Image src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-                               alt=""
-                               width={400}
-                               height={250}
-                               className={styles.image}
-                        />
-                    </div>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}> test</h1>
-                        <p className={styles.desc}>desc</p>
-
-                    </div>
-                </Link>
             </div>
         </>
     )
